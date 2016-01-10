acd24025e76d6131f28c3fb10c37c59e008e308d
senthilshravan <senthilshravanbala@gmail.com>
2016-01-10 13:47:48 +0530

    Added readme and proxsetup.sh

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Linux-Proxy
 A shell file to setup up proxy each time instead of going to Network and specifying proxy 
+To configure proxy , copy proxsetup.sh file to /usr/bin folder
+
+Usage:
+  To add proxy, run proxsetup.sh 1 on the terminal.
+  To remove proxy, run proxsetup.sh 2 on the terminal. 
---
+++ proxsetup.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+PROXY="proxy.ssn.net"
+PORT=8080
+
+setproxy(){
+  echo "Setting Proxy"
+  gsettings set org.gnome.system.proxy mode 'manual'
+  gsettings set org.gnome.system.proxy.http host $PROXY
+  gsettings set org.gnome.system.proxy.http port $PORT
+  gsettings set org.gnome.system.proxy.https host $PROXY
+  gsettings set org.gnome.system.proxy.https port $PORT
+  gsettings set org.gnome.system.proxy.ftp host $PROXY
+  gsettings set org.gnome.system.proxy.ftp port $PORT
+  gsettings set org.gnome.system.proxy.socks host $PROXY
+  gsettings set org.gnome.system.proxy.socks port $PORT
+
+}
+
+unsetproxy(){
+  gsettings set org.gnome.system.proxy mode 'none'
+  echo "Proxy Unset Successful"
+}
+
+checkproxy(){
+  noflines="$(env | grep $PROXY | wc -l)"
+  if [ $noflines -eq 0 ]
+  then
+  echo "Proxy is not Set"
+  else
+  echo "Proxy is set"
+  fi
+}
+
+if [ $1 -eq 1 ]
+then
+setproxy
+elif [ $1 -eq 2 ]
+then
+  unsetproxy
+else
+  checkproxy
+fi
